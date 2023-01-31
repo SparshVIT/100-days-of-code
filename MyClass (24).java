ad985183fb9290e3149f28ae8ccb7e101266347d
SparshVIT <80635277+SparshVIT@users.noreply.github.com>
2023-01-31 10:45:14 +0530

    Add files via upload

---
+++ MyClass (24).java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        if(num%2!=0){
+            return false;
+        }
+        for(int i=1;i<=num/2;i++){
+            if(num%i==0){
+                sum = sum+i;
+            }
+        }
+        if(sum!=num){
+            return false;
+        }
+        return true;
+    }
+}
+public class MyClass {
+    public static void main(String args[]) {
+      Solution s = new Solution();
+    }
+}
