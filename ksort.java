8d56c9d0be88908a5f76bc7c78647a3f1e676c71
SparshVIT <80635277+SparshVIT@users.noreply.github.com>
2023-02-04 10:33:40 +0530

    Create ksort.java

---
+++ ksort.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        ArrayList<Integer> a = new ArrayList<>();
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                a.add(matrix[i][j]);
+            }
+        }
+        Collections.sort(a);
+        return a.get(k-1);
+    }
+}
