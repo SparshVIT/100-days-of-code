a5b1bae0334a7c42a04c9f2eb1824b619268c6d5
Sparsh Jain <sparshjainanjali@gmail.com>
2023-10-03 21:26:09 +0530

    Time: 2 ms (53.08%), Space: 45 MB (89.25%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
@@ -1,24 +1,20 @@
 class Solution {
     List<List<Integer>> l = new ArrayList<>();
-    public void find(int[][] g, int d, List<Integer> l1){
-        if(d>=g.length){
-            return;
+    public void dfs(int[][] g, int s, List<Integer> l1, boolean visit[]){
+        visit[s] = true;
+        l1.add(s);
+        for(int v: g[s]){
+            dfs(g,v,new ArrayList<>(l1),visit);
         }
-        if(d==g.length-1){
+        if(l1.get(l1.size()-1)==g.length-1){
             l.add(l1);
-            return;
-        }
-        for(int i=0;i<g[d].length;i++){
-            l1.add(g[d][i]);
-            find(g,g[d][i],new ArrayList<>(l1));
-            l1.remove(l1.size()-1);
         }
     }
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         int d = 0;
         List<Integer> l1 = new ArrayList<>();
-        l1.add(0);
-        find(graph,d,l1);
+        boolean visit[] = new boolean[graph.length];
+        dfs(graph,0,l1,visit);
         return l;
     }
 }
